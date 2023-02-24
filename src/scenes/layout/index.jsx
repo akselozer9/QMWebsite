92761806d05e45e98b455821d118eced1163c561
Aksel Ozer <akselozer9@unm.edu>
2023-02-23 23:51:44 -0700

    Create index.jsx

---
+++ src/scenes/layout/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "/Users/akselozer/qmdashboard/qmdashboard/src/components/Navbar.jsx";
+import Sidebar from "/Users/akselozer/qmdashboard/qmdashboard/src/components/Sidebar.jsx";
+
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <Sidebar
+    
+        isNonMobile={isNonMobile}
+        drawerWidth="200px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box flexGrow={1}>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
